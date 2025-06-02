9907dfdb364a3bc88f8130095d0680f8585f262f
ChandruV2003 <chandruvasu@icloud.com>
2025-06-02 11:21:27 -0400

    Initial Commit

---
+++ Instrument Cluster/Instrument_ClusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instrument_ClusterApp.swift
+//  Instrument Cluster
+//
+//  Created by Chandru Vasudevan on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Instrument_ClusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Instrument ClusterTests/Instrument_ClusterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Instrument_ClusterTests.swift
+//  Instrument ClusterTests
+//
+//  Created by Chandru Vasudevan on 6/2/25.
+//
+
+import Testing
+@testable import Instrument_Cluster
+
+struct Instrument_ClusterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
